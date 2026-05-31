e5f5822c14f49048473784cb3cc4dddc9c3010a2
JakubSiwek213 <s10925js@ms.wwsi.edu.pl>
2026-05-31 13:32:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using System;
+
+namespace projekt_Jakub_Siwek_Z403_AV
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
+}
